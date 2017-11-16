1ff63a7bb86c045b4edbd60e9f5f97d19a646163
Ashley Fisher <fish.ash@gmail.com>
2017-11-15 18:12:09 -0800

    version 0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   name = 'hoursofoperation',
   packages = ['hoursofoperation'],
   version = __version__,
-  description = 'Utilities for loading and doing calculations with a partner\'s hours of operations configration.'
+  description = 'Utilities for loading and doing calculations with a partner\'s hours of operations configration.',
   author = 'Ashley Fisher',
   author_email = 'fish.ash@gmail.com',
   url = 'https://github.com/Brightmd/hoursofoperation',
