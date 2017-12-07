bbb45f3aace0929a5331f5c0aa22f5dc761fe7b9
Ashley Fisher <fish.ash@gmail.com>
2017-12-07 11:48:53 -0800

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from inspect import cleandoc
 
 from hoursofoperation._version import __version__
 
@@ -13,4 +14,10 @@ setup(
   url = 'https://github.com/Brightmd/hoursofoperation',
   keywords = ['hours'],
   classifiers = [],
+  scripts = ['bin/hoursofoperation'],
+    install_requires=cleandoc('''
+      codado>=0.4.997,<0.6
+      python-dateutil==2.4.0
+      pytz==2015.4
+      ''').split()
 )
