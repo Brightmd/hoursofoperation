96e8b31bc9f8915b944ff3e8e5e174bcea64fe10
Cory Dodt <121705+corydodt@users.noreply.github.com>
2019-12-09 16:47:49 -0800

    release-0.1.9 clean up tox and dependencies

--- README.md
+++ README.md
@@ -14,6 +14,10 @@ twine upload dist/*<version>*
 ```
 
 ## Change Log
+### [0.1.9] - 2019-12-09
+#### Added
+- Python 3.6+ support!
+
 ### [0.1.8] - 2018-10-05
 #### Added
 - Python 3.6+ support!
@@ -30,6 +34,7 @@ twine upload dist/*<version>*
 #### Changed
 - Minor stylistic updates
 
+[0.1.9]: https://github.com/Brightmd/hoursofoperation/compare/release-0.1.8...release-0.1.9
 [0.1.8]: https://github.com/Brightmd/hoursofoperation/compare/0.1.7...release-0.1.8
 [0.1.7]: https://github.com/Brightmd/hoursofoperation/compare/0.1.6...0.1.7
 [0.1.6]: https://github.com/Brightmd/hoursofoperation/compare/0.1.5...0.1.6
--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
-from inspect import cleandoc
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 _version = {}
 exec(open('hoursofoperation/_version.py').read(), _version)
 
 
-setup(
-  name = 'hoursofoperation',
-  packages = ['hoursofoperation', 'hoursofoperation.test'],
-  version = _version['__version__'],
-  description = 'Utilities for loading and doing calculations with a partner\'s hours of operations configration.',
-  author = 'Ashley Fisher',
-  author_email = 'fish.ash@gmail.com',
-  url = 'https://github.com/Brightmd/hoursofoperation',
-  keywords = ['hours'],
-  classifiers = [],
-  scripts = [],
-  install_requires=cleandoc('''
-    codado>=0.6,<0.7
-    python-dateutil==2.4.0
-    pytz==2015.4
-    ''').split()
+config = dict(
+    name='hoursofoperation',
+    packages=find_packages('.', include=['hoursofoperation.*']),
+    version=_version['__version__'],
+    description = 'Utilities for loading and doing calculations with a partner\'s hours of operations configration.',
+    author='Ashley Fisher',
+    author_email='fish.ash@gmail.com',
+    url='https://github.com/Brightmd/hoursofoperation',
+    keywords=['hours'],
+    classifiers=[],
+    scripts=[],
+    install_requires=[
+        'codado',
+        'python-dateutil',
+        'pytz',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-coverage',
+            'pytest-flakes',
+            'tox',
+    ]},
 )
+
+
+setup(**config)
